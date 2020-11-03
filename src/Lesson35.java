3b55a872268408ce1e0f74906399733b4eb88c54
gui66497 <gui66497@hotmail.com>
2020-11-03 23:32:18 +0800

    Lesson35 搜索插入位置

---
+++ src/Lesson35.java
@@ -0,0 +1,47 @@
+// 35. 搜索插入位置
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+// 你可以假设数组中无重复元素。
+
+// 示例 1:
+
+// 输入: [1,3,5,6], 5
+// 输出: 2
+// 示例 2:
+
+// 输入: [1,3,5,6], 2
+// 输出: 1
+// 示例 3:
+
+// 输入: [1,3,5,6], 7
+// 输出: 4
+// 示例 4:
+
+// 输入: [1,3,5,6], 0
+// 输出: 0
+
+class Solution35 {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        int res = nums.length;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] >= target) {
+                res = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 2, 3, 5, 6 };
+        Solution35 solution = new Solution35();
+        int res = solution.searchInsert(nums, 1);
+        System.out.println("res:" + res);
+    }
+}
