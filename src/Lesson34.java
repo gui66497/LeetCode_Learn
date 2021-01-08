08c18e45fd935309513726e18fdd3e14e4264b25
gui66497 <gui66497@hotmail.com>
2021-01-08 10:47:53 +0800

    Lesson34 在排序数组中查找元素的第一个和最后一个位置

---
+++ src/Lesson34.java
@@ -0,0 +1,88 @@
+// 34. 在排序数组中查找元素的第一个和最后一个位置
+// 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+// 如果数组中不存在目标值 target，返回 [-1, -1]。
+
+// 进阶：
+
+// 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+ 
+
+// 示例 1：
+
+// 输入：nums = [5,7,7,8,8,10], target = 8
+// 输出：[3,4]
+// 示例 2：
+
+// 输入：nums = [5,7,7,8,8,10], target = 6
+// 输出：[-1,-1]
+// 示例 3：
+
+// 输入：nums = [], target = 0
+// 输出：[-1,-1]
+ 
+
+// 提示：
+
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// nums 是一个非递减数组
+// -109 <= target <= 109
+
+class Solution34 {
+    public int[] searchRange(int[] nums, int target) {
+        int len = nums.length;
+        if (len == 0) {
+            return new int[] { -1, -1 };
+        }
+        int left = findFirst(nums, target);
+        int right = findLast(nums, target);
+
+        return new int[] { left, right };
+    }
+
+    //第一次出现位置
+    int findFirst(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+
+    //最后出现位置
+    int findLast(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            //这里要向上取整 否则死循环
+            int mid = (left + right + 1) >>> 1;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                left = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 1, 2, 3, 3, 4 };
+        Solution34 solution = new Solution34();
+        int[] res = solution.searchRange(nums, 3);
+        System.out.println(res);
+    }
+}
