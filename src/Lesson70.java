0cf32c4fab38e0c094b0bdd0e9279cbf8abaddf7
gui66497 <gui66497@hotmail.com>
2021-02-20 16:37:49 +0800

    Lesson70 爬楼梯 增加动态规划解法

--- src/Lesson70.java
+++ src/Lesson70.java
@@ -21,7 +21,7 @@
 // 2.  1 阶 + 2 阶
 // 3.  2 阶 + 1 阶
 
-class Solution70 {
+class Lesson70 {
 
     public int climbStairs(int n) {
         if (n < 1) {
@@ -38,9 +38,25 @@ class Solution70 {
         return memos[n];
     }
 
+    public int climbStairs2(int n) {
+        // 动态规划
+        if (n < 1) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
-        Solution70 solution = new Solution70();
+        Lesson70 solution = new Lesson70();
         int res = solution.climbStairs(4);
         System.out.println("res:" + res);
+        int res2 = solution.climbStairs2(4);
+        System.out.println("res2:" + res2);
     }
 }
