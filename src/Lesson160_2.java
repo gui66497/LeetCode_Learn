29e1887049cb5946a44d8ff7f727498321e4d04b
fgt <664973827@qq.com>
2024-02-29 22:56:13 +0800

    Lesson160 相交链表 202402

---
+++ src/Lesson160_2.java
@@ -0,0 +1,23 @@
+public class Lesson160_2 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        Lesson160_2 solution = new Lesson160_2();
+        ListNode res = solution.getIntersectionNode(head, head);
+        System.out.println(res);
+    }
+}
