09c76c8692a9f7f0c83113db81d2b864fffd0214
fgt <664973827@qq.com>
2024-03-06 15:30:29 +0800

    Lesson139 单词拆分

---
+++ src/Lesson139.java
@@ -0,0 +1,63 @@
+// 139. 单词拆分
+// 中等
+
+// 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。如果可以利用字典中出现的一个或多个单词拼接出 s 则返回 true。
+
+// 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+
+// 示例 1：
+
+// 输入: s = "leetcode", wordDict = ["leet", "code"]
+// 输出: true
+// 解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+// 示例 2：
+
+// 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+// 输出: true
+// 解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+//      注意，你可以重复使用字典中的单词。
+// 示例 3：
+
+// 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+// 输出: false
+
+// 提示：
+
+// 1 <= s.length <= 300
+// 1 <= wordDict.length <= 1000
+// 1 <= wordDict[i].length <= 20
+// s 和 wordDict[i] 仅由小写英文字母组成
+// wordDict 中的所有字符串 互不相同
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Lesson139 {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        // 动态规划
+        // dp[i] = dp[j]&&check(j~i)
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                //前i个字符满足 
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Lesson139 solution = new Lesson139();
+        List<String> wordDict = new ArrayList<>();
+        wordDict.add("leet");
+        wordDict.add("code");
+        boolean res = solution.wordBreak("leetcodeleet", wordDict);
+        System.out.println(res);
+    }
+}
