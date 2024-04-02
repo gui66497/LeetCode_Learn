782f492a971bc9d99d1122c17932a84e7577a5c2
fgt <664973827@qq.com>
2024-04-02 16:07:31 +0800

    Lesson62_2 不同路径

--- src/Lesson199.java
+++ src/Lesson199.java
@@ -29,6 +29,9 @@ public class Lesson199 {
     //BFS 利用 BFS 进行层次遍历，记录下每层的最后一个元素。
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         while (!queue.isEmpty()) {
