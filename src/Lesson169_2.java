1dfc16683e2edd123d61907c8e8fcf0794fed1be
fgt <664973827@qq.com>
2024-04-25 17:28:11 +0800

    Lesson169_2 多数元素

---
+++ src/Lesson169_2.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+/*169. 多数元素
+
+简单
+
+给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+示例 1：
+
+输入：nums = [3,2,3]
+输出：3
+示例 2：
+
+输入：nums = [2,2,1,1,1,2,2]
+输出：2
+
+提示：
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+
+进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。*/
+
+class Lesson169_2 {
+
+    //排序 然后取中值
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+    public static void main(String[] args) {
+        Lesson169_2 solution = new Lesson169_2();
+        int res = solution.majorityElement(new int[]{10, 9, 9, 9, 10});
+        System.out.println("res:" + res);
+    }
+}
